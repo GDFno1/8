802a419450a1521cf07bc56411113b965e6a60e2
GDFno1 <74794051+GDFno1@users.noreply.github.com>
2020-12-06 16:11:52 +0200

    Add project files.

---
+++ 8/CodeFile1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class UngaBung
+{
+    static void Main()
+    {
+        int Coliches;
+        int temp;
+        int current = 1;
+        int prev = 0;
+
+        Console.WriteLine("Enter a col of teh number checking!");
+
+        Coliches = Int32.Parse(Console.ReadLine());
+        Console.Clear();
+
+        while (Coliches>0)
+        {
+            int[] Something = new int[1];
+
+            Coliches--;
+        }
+
+        if (new int[1] % 5 = 2)
+        {
+
+        }
+        
+
+        
+    }
+}
